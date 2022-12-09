6c8a0f827866edf1949b8acee5854af0c22db66b
Memo Chou <memochou1993@gmail.com>
2022-12-09 16:44:50 +0800

    Add openai api

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-param-reassign': 'off',
+  },
+};
