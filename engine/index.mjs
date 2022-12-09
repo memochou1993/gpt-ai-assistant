a114872de82bdb2b629329508810b0fce8d7400a
Memo Chou <memochou1993@gmail.com>
2022-12-09 18:40:46 +0800

    Add line api

---
+++ engine/index.mjs
@@ -0,0 +1,21 @@
+import {
+  TITLE_AI,
+  FINISH_REASON_STOP,
+  prompt,
+} from '../services/openai.mjs';
+
+export const messages = [
+  `${TITLE_AI}: 嗨！我可以怎麼幫助你？`,
+];
+
+export const chat = async ({ context, reply = '' }) => {
+  const { data } = await prompt(context);
+  const [choice] = data.choices;
+  const text = choice.text.trim();
+  context += text;
+  reply += text.replace(`${TITLE_AI}: `, '');
+  const res = { context, reply };
+  return choice.finish_reason === FINISH_REASON_STOP ? res : chat(res);
+};
+
+export default null;
--- services/openai.mjs
+++ services/openai.mjs
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
-
-dotenv.config();
+import config from '../config/index.mjs';
 
 export const TITLE_AI = 'AI';
 export const TITLE_HUMAN = 'Human';
@@ -9,12 +7,12 @@ export const FINISH_REASON_STOP = 'stop';
 export const FINISH_REASON_LENGTH = 'length';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: config.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const prompt = (context) => openai.createCompletion({
+export const prompt = (context) => openai.createCompletion({
   model: 'text-davinci-003',
   prompt: context,
   temperature: 0.9,
@@ -27,13 +25,3 @@ const prompt = (context) => openai.createCompletion({
     ` ${TITLE_HUMAN}:`,
   ],
 });
-
-export const chat = async ({ context, reply = '' }) => {
-  const { data } = await prompt(context);
-  const [choice] = data.choices;
-  const text = choice.text.trim('\n');
-  context += text;
-  reply += text;
-  const res = { context, reply };
-  return choice.finish_reason === FINISH_REASON_STOP ? res : chat(res);
-};
