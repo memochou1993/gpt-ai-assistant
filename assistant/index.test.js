6b62ebc49fe131017271fe79af2608495cbfa134
Memo Chou <memochou1993@gmail.com>
2022-12-10 18:40:54 +0800

    Add test cases

---
+++ assistant/index.test.js
@@ -0,0 +1,34 @@
+import {
+  expect,
+  test,
+} from '@jest/globals';
+import Assistant from './index.js';
+
+test('assistant works', async () => {
+  const assistant = new Assistant();
+  const events = [
+    {
+      type: 'message',
+      message: {
+        text: '嗨',
+      },
+      source: {
+        type: 'user',
+        userId: '00000',
+      },
+      replyToken: '',
+    },
+  ];
+  const res = await assistant.handleEvents(events);
+  assistant.debug();
+  expect(res).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        messages: expect.arrayContaining([{
+          text: expect.any(String),
+          type: 'text',
+        }]),
+      }),
+    ]),
+  );
+});
