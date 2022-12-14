9283ec0579a5eb712c6eb1e2fd1a9c40c379018f
Memo Chou <memochou1993@gmail.com>
2022-12-15 04:22:48 +0800

    Remove release note due to remote version cached

--- utils/fetch-version.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const fetchVersion = async () => {
-  const url = 'https://raw.githubusercontent.com/memochou1993/gpt-ai-assistant/main/package.json';
-  const { data } = await axios.get(url);
-  return data.version;
-};
-
-export default fetchVersion;
