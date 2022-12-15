387c9b1ba1ad1c448143cb342cc4f564df6eb937
Memo Chou <memochou1993@gmail.com>
2022-12-15 13:22:41 +0800

    Rename stop sequences

--- services/openai/index.js
+++ services/openai/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import config from '../../config/index.js';
 
-export const PARTICIPANT_AI = 'A';
-export const PARTICIPANT_HUMAN = 'Q';
+export const PARTICIPANT_AI = 'AI';
+export const PARTICIPANT_HUMAN = 'Human';
 export const FINISH_REASON_STOP = 'stop';
 export const FINISH_REASON_LENGTH = 'length';
 
