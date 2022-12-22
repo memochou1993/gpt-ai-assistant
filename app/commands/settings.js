dd38ed03c23c24f57f1363cc290468930833f83c
Memo Chou <memochou1993@gmail.com>
2022-12-23 03:19:29 +0800

    Rename setting

--- app/commands/settings.js
+++ app/commands/settings.js
@@ -1,7 +1,7 @@
 import {
   COMMAND_ACTIVATE, COMMAND_DEACTIVATE, COMMAND_DEPLOY, COMMAND_SETTINGS, COMMAND_VERSION,
 } from '../../constants/command.js';
-import { SETTING_BOT_ACTIVATED } from '../../constants/setting.js';
+import { SETTING_AI_ACTIVATED } from '../../constants/setting.js';
 import storage from '../../storage/index.js';
 import { MessageAction } from '../actions/index.js';
 import Event from '../event.js';
@@ -19,7 +19,7 @@ const isSettings = (event) => event.isCommand(COMMAND_SETTINGS);
 const execSettingsCommand = async (event) => {
   event.sendTemplate(COMMAND_SETTINGS.label, [
     new MessageAction(COMMAND_VERSION),
-    new MessageAction(await storage.getItem(SETTING_BOT_ACTIVATED) ? COMMAND_DEACTIVATE : COMMAND_ACTIVATE),
+    new MessageAction(await storage.getItem(SETTING_AI_ACTIVATED) ? COMMAND_DEACTIVATE : COMMAND_ACTIVATE),
     new MessageAction(COMMAND_DEPLOY),
   ]);
   return event;
