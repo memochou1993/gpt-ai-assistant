92415c91d8bcda0496282e9e6b76ba3cfebc0ad7
Memo Chou <memochou1993@gmail.com>
2022-12-31 15:16:16 +0800

    Update validateLineSignature middleware

--- middleware/validate-line-signature.js
+++ middleware/validate-line-signature.js
@@ -1,13 +1,10 @@
 import config from '../config/index.js';
-import {
-  validateSignature,
-} from '../utils/index.js';
+import { validateSignature } from '../utils/index.js';
 
 const validateLineSignature = (req, res, next) => {
-  const { rawBody } = req;
-  const { LINE_CHANNEL_SECRET } = config;
+  const secret = config.LINE_CHANNEL_SECRET || '';
   const signature = req.header('x-line-signature');
-  if (LINE_CHANNEL_SECRET && !validateSignature(rawBody, LINE_CHANNEL_SECRET, signature)) {
+  if (!validateSignature(req.rawBody, secret, signature)) {
     res.sendStatus(403);
     return;
   }
