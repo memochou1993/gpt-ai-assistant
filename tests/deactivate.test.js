675cba4639a56805e6e5e825c82b88b28c3ad692
Memo Chou <memochou1993@gmail.com>
2023-01-04 02:24:10 +0800

    Update tests

--- tests/deactivate.test.js
+++ tests/deactivate.test.js
@@ -2,10 +2,12 @@ import {
   afterEach, beforeEach, expect, test,
 } from '@jest/globals';
 import { getPrompt, handleEvents, removePrompt } from '../app/index.js';
-import { COMMAND_DEACTIVATE } from '../constants/command.js';
+import { COMMAND_ACTIVATE, COMMAND_DEACTIVATE } from '../constants/command.js';
 import { t } from '../locales/index.js';
 import storage from '../storage/index.js';
-import { createEvents, MOCK_USER_01, TIMEOUT } from './utils.js';
+import {
+  createEvents, MOCK_TEXT_OK, MOCK_USER_01, TIMEOUT,
+} from './utils.js';
 
 beforeEach(() => {
   //
@@ -17,6 +19,8 @@ afterEach(() => {
 
 test('COMMAND_DEACTIVATE', async () => {
   const events = [
+    ...createEvents([COMMAND_ACTIVATE.text]),
+    ...createEvents(['嗨！']),
     ...createEvents([COMMAND_DEACTIVATE.text]),
     ...createEvents(['嗨！']), // should be ignored
   ];
@@ -26,10 +30,15 @@ test('COMMAND_DEACTIVATE', async () => {
   } catch (err) {
     console.error(err);
   }
-  expect(getPrompt(MOCK_USER_01).sentences.length).toEqual(1);
+  expect(getPrompt(MOCK_USER_01).sentences.length).toEqual(3);
   const replies = results.map(({ messages }) => messages.map(({ text }) => text));
   expect(replies).toEqual(
     [
+      [
+        t('__ERROR_MISSING_ENV')('VERCEL_ACCESS_TOKEN'),
+        COMMAND_ACTIVATE.reply,
+      ],
+      [MOCK_TEXT_OK],
       [
         t('__ERROR_MISSING_ENV')('VERCEL_ACCESS_TOKEN'),
         COMMAND_DEACTIVATE.reply,
