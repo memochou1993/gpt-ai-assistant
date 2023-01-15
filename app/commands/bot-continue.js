69bd4cdf3d8d9cb0f9e294f482b2702c81988977
Memo Chou <memochou1993@gmail.com>
2023-01-15 12:19:33 +0800

    Add command aliases

--- app/commands/bot-continue.js
+++ app/commands/bot-continue.js
@@ -7,6 +7,7 @@ export default new Command({
   label: t('__COMMAND_BOT_CONTINUE_LABEL'),
   text: t('__COMMAND_BOT_CONTINUE_TEXT'),
   aliases: [
+    ...t('__COMMAND_BOT_CONTINUE_ALIASES'),
     '/continue',
     'Continue',
   ],
