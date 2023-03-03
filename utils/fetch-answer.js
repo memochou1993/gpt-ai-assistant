4ec8c4eb71f82e9e83222ae6149b8e35daf3d3ef
Memo Chou <memochou1993@gmail.com>
2023-03-03 22:51:31 +0800

    Optimize search command

--- utils/fetch-answer.js
+++ utils/fetch-answer.js
@@ -14,10 +14,12 @@ class OrganicResult {
 const fetchAnswer = async (q) => {
   if (config.APP_ENV !== 'production' || !config.SERPAPI_API_KEY) return new OrganicResult();
   const res = await search({ q });
-  const { answer_box: answerBox, organic_results: organicResults } = res.data;
+  const { answer_box: answerBox, knowledge_graph: knowledgeGraph, organic_results: organicResults } = res.data;
   let answer = organicResults[0].snippet;
+  if (answerBox?.answer) answer = answerBox.answer;
   if (answerBox?.result) answer = answerBox.result;
   if (answerBox?.snippet) answer = answerBox.snippet;
+  if (knowledgeGraph?.description) answer = `${knowledgeGraph.title} - ${knowledgeGraph.description}`;
   return new OrganicResult({ answer });
 };
 
