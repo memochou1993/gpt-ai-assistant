6169c2b95d55f780e14579561edcf0a6319d4d20
Memo Chou <memochou1993@gmail.com>
2023-03-05 14:54:29 +0800

    Update add mark helper

--- utils/add-mark.js
+++ utils/add-mark.js
@@ -1,9 +1,12 @@
+import config from '../config/index.js';
+
 const addMark = (text) => {
   const marks = ['？', '。', '！', '?', '.', '!'];
   if (marks.some((mark) => text.endsWith(mark))) {
     return text;
   }
-  return `${text}.`;
+  if (config.APP_LANG === 'zh' || config.APP_LANG === 'ja') return '。';
+  return '.';
 };
 
 export default addMark;
